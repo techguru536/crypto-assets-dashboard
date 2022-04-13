7e3a63b4db622cc84f8eb62ec2ab22340973e95e
techguru536 <techguru95922@gmail.com>
2022-04-13 13:57:12 -0400

    Update crypto_streamlit_app.py

--- crypto_streamlit_app.py
+++ crypto_streamlit_app.py
@@ -18,7 +18,7 @@ hv.extension('bokeh')
 # Insert your private key into a .env file. 
 # A paid subscription is required.
 
-st.write("messari_api_key:", st.secrets["MESSARI_API_KEY"])
+#st.write("messari_api_key:", st.secrets["MESSARI_API_KEY"])
 st.write(
     "Has environment variables been set:",
     os.environ["MESSARI_API_KEY"] == st.secrets["MESSARI_API_KEY"],
