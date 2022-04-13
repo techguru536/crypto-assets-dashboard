a3b016196bf248988b693ba961e1c9846e544485
techguru536 <techguru95922@gmail.com>
2022-04-12 21:02:44 -0400

    Update crypto_streamlit_app.py

--- crypto_streamlit_app.py
+++ crypto_streamlit_app.py
@@ -18,7 +18,7 @@ load_dotenv()
 # Insert your private key into a .env file. 
 # A paid subscription is required.
 
-st.write(os.environ["messari_api_key"], st.secrets["MESSARI_API_KEY"]
+st.write(os.environ["messari_api_key"] == st.secrets["MESSARI_API_KEY"]
 #messari_api_key = os.getenv("MESSARI_API_KEY")
 messari = Messari(messari_api_key)
 
