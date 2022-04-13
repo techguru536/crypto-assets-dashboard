45815528485e88716633905cc32fe7cb3a72c69d
techguru536 <techguru95922@gmail.com>
2022-04-13 14:56:18 -0400

    Update crypto_streamlit_app.py

--- crypto_streamlit_app.py
+++ crypto_streamlit_app.py
@@ -19,10 +19,6 @@ hv.extension('bokeh')
 # A paid subscription is required.
 
 #st.write("messari_api_key:", st.secrets["MESSARI_API_KEY"])
-st.write(
-    "Has environment variables been set:",
-    os.environ["MESSARI_API_KEY"] == st.secrets["MESSARI_API_KEY"],
-)
 
 messari_api_key = os.environ["MESSARI_API_KEY"]
 messari = Messari(messari_api_key)
